9ca851c558cf766188291cdba6de8574ab69b821
marialwj <mariaa.georgy@gmail.com>
2024-03-27 19:56:35 -0700

    env file created, fixed mysql errors to run on insomnia, still running into errors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection');
 // import sequelize connection
 
 const app = express();
@@ -9,8 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
