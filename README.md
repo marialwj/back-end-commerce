7b3079386e362dc6e1492f1f7c2fbe23381aa08b
marialwj <mariaa.georgy@gmail.com>
2024-03-28 00:31:26 -0700

    completed

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# back-end-commerce
+# E-Commerce Backend 🛒
+
+This project implements a backend for an e-commerce site using Express.js API to use Sequelize to interact with a MySQL database. This backend allows for managing products, categories, and tags associated with the products.
+
+## Features 🌟
+
+- RESTful API for managing e-commerce entities including products, categories, and tags.
+- CRUD operations for each entity, allowing for creation, reading, updating, and deletion of products, categories, and tags.
+- Sequelize ORM for database management.
+- Secure connection to MySQL database using environment variables.
+
+## Technologies Used 💻
+
+- Node.js
+- Express.js
+- MySQL
+- Sequelize ORM
+- dotenv for environment variables management
+
+## Getting Started 🚀
+
+### Prerequisites
+
+- Node.js
+- MySQL
+- npm (Node Package Manager)
+
+### Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/marialwj/back-end-commerce
+```
+
+2. 
+```bash
+npm install
+```
+3. 
+```bash
+npm run seed
+```
+4. 
+```bash
+node sever.js
+```
+## Usage 📋
+The API can be accessed on http://localhost:3001/api/ and includes the following endpoints:
+
+1. /api/products for products CRUD operations.
+2. /api/categories for categories CRUD operations.
+/api/tags for tags CRUD operations.
+Use a tool like Insomnia or Postman to test the API endpoints.
+
+## Walkthrough
+![Walkthrough](ecommerce-walkthrough.mp4)
+
+
+## License 📄
+N/A
--- models/Product.js
+++ models/Product.js
@@ -1,44 +1,42 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
+    // Define columns for the product table
     id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true, 
     },
     product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      type: DataTypes.STRING, 
+      allowNull: false, 
     },
     price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
       validate: {
-        isDecimal: true,
+        isDecimal: true, 
       },
     },
     stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: 10, 
       validate: {
-        isNumeric: true,
+        isNumeric: true, 
       },
     },
     category_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, 
       references: {
-        model: 'category',
-        key: 'id' 
+        model: "category",
       },
     },
   },
@@ -47,7 +45,7 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
